050da5e6c5dc454269c957f1dd3b230e7679ac12
nandini <nininandini@gmail.com>
2023-06-19 12:07:54 -0700

    Add header details

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import CTA from './CTA'
+import ME from '../../assets/me.png'
+import HeaderSocials from './HeaderSocials'
 
 import './header.css'
 
@@ -8,10 +11,16 @@ const Header = () => {
   <header>
     <div className='container header__container'>
     <h5>Hello I'm</h5>
-    <h1>Nandini a </h1>
+    <h1>Nandini Bag </h1>
     <h5 className='text-light'>Frontend Developer</h5> 
+    <CTA/>
+    <HeaderSocials/>
+    <div className='me'>
+    <img src={ME} alt='me'/> 
+    </div>
+    <a href='#contact' className='scroll__down'>Scroll Down</a>
    </div>
-    </header>
+   </header>
   )
 }
 
