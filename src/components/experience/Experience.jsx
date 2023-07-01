1b08aa90f686d45f903d76728c0e67c6d34ba8bc
nandini <nininandini@gmail.com>
2023-07-01 06:01:02 -0700

    Add Experience component

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,9 +1,56 @@
 import React from 'react'
 import './experience.css'
+import {BsPatchCheckFill} from "react-icons/bs"
 
 const Experience = () => {
   return (
-    <section id="experience">Experience</section>
+    <section id="experience">Experience
+    <h5>What Skills I have</h5>
+    <h2>My Experience</h2>
+    <div className='container experience__container'>
+      <div className="experience__frontend">
+        <h3>FrontEnd Development</h3>
+        <div className="experience__content">
+          <article className='experience__details'>
+             <BsPatchCheckFill/>
+             <h4>HTML</h4>
+             <small className='text-light'>Experienced</small>
+          </article>
+          <article className='experience__details'>
+             <BsPatchCheckFill/>
+             <h4>CSS</h4>
+             <small className='text-light'>Intermediate</small>
+          </article>
+          <article className='experience__details'>
+             <BsPatchCheckFill/>
+             <h4>Javascript</h4>
+             <small className='text-light'>Experienced</small>
+          </article>
+          <article className='experience__details'>
+             <BsPatchCheckFill/>
+             <h4>React</h4>
+             <small className='text-light'>Experienced</small>
+          </article>
+        </div>
+      </div>
+      <div className="experience__frontend">
+        <h3>BackEnd Development</h3>
+        <div className="experience__content">
+          <article className='experience__details'>
+             <BsPatchCheckFill/>
+             <h4>MySQL</h4>
+             <small className='text-light'>Experienced</small>
+          </article>
+          <article className='experience__details'>
+             <BsPatchCheckFill/>
+             <h4>NodeJs</h4>
+             <small className='text-light'>Intermediate</small>
+          </article>
+        </div>
+      </div>
+     </div>
+
+    </section>
   )
 }
 
