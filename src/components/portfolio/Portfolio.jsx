6a9469d46f510b453b72c0f2bb3d98fb5869ee55
nandini <nininandini@gmail.com>
2023-07-24 19:44:13 -0700

    Add portfolio section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,7 +3,17 @@ import './portfolio.css'
 
 const Portfolio = () => {
   return (
-    <section id="portfolio">Portfolio</section>
+    <section id="portfolio">
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+     <div className='container portfolio__container'>
+      <article className='portfolio__item'>
+        <div className='portfolio__item-image'></div>
+        <h3>This is title</h3>
+      </article>
+     </div>
+    </section>
   )
 }
 
